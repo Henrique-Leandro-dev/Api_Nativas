4332193f11ba8f9eaccd1c0342c211216605660d
Henrique Leandro <miniqui10@gmail.com>
2020-11-23 03:55:00 -0200

    Api nativas Finalizadas

---
+++ App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Contatos from './pages/Contatos';
+import TextToSpeech from './pages/Speech';
+import ImagemCamera from './pages/ImagemCamera';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Contatos" component={Contatos}/>
+        <Tab.Screen name="TextToSpeech" component={TextToSpeech} />
+        <Tab.Screen name="ImagemCamera" component={ImagemCamera}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
